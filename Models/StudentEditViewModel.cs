e10a8fd987b24d114a6cff3b6c63d5c17047f76b
Владислав <vlad2007gvv@gmail.com>
2026-05-20 15:42:06 +0300

    добавлен StudentEditViewModel

---
+++ Models/StudentEditViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalAccount.Models
+{
+    public class StudentEditViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string FullName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "GroupName is required")]
+        public string GroupName { get; set; } = string.Empty;
+        public string? PhotoUrl { get; set; }
+    }
+}
