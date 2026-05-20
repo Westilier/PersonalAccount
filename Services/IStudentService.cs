ab5dbac17821e6389fa727b28358f5456a09a8ca
Владислав <vlad2007gvv@gmail.com>
2026-05-20 17:03:18 +0300

    Добавлен StudentService Добавлен метод Обновления данных пользователя

---
+++ Services/IStudentService.cs
@@ -0,0 +1,9 @@
+﻿using PersonalAccount.Models.Students;
+
+namespace PersonalAccount.Services
+{
+    public interface IStudentService
+    {
+        Task UpdateByIdAsync(int id, StudentModel student);
+    }
+}
---
+++ Services/StudentService.cs
@@ -0,0 +1,14 @@
+﻿using PersonalAccount.Models.Students;
+using PersonalAccount.Repository;
+
+namespace PersonalAccount.Services
+{
+    public class StudentService(IStudentRepo<StudentModel> students) : IStudentService
+    {
+
+        public async Task UpdateByIdAsync(int id, StudentModel student)
+        {
+            await students.UpdateByIdAsync(id, student);
+        }
+    }
+}
