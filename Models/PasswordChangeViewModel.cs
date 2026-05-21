508f7844fd1d65e79af35d50a4077f69aae7c057
Владислав <vlad2007gvv@gmail.com>
2026-05-21 21:43:29 +0300

    Добавлена модель для View изменения пароля

---
+++ Models/PasswordChangeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalAccount.Models
+{
+    public class PasswordChangeViewModel
+    {
+        [Required(ErrorMessage = "OldPassword field is required")]
+        public string OldPassword { get; set; } = string.Empty;
+        [Required(ErrorMessage = "NewPassword field is required")]
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
