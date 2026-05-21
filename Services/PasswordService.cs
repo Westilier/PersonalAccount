95f5f75a122f5973eb4973fd5bfc4b6c182f9d23
Владислав <vlad2007gvv@gmail.com>
2026-05-21 21:47:16 +0300

    Добавлен сервис для изменения пароля

---
+++ Services/PasswordService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PersonalAccount.Models.Students;
+using PersonalAccount.Repository;
+using System.Web.Helpers;
+
+namespace PersonalAccount.Services
+{
+    public class PasswordService(IStudentRepo<StudentAuthModel> students, IPasswordHasher<StudentAuthModel> hasher) : IPasswordService
+    {
+
+        public async Task<bool> ValidatePasswordAsync(int id, string password)
+        {
+            var student = await students.GetByIdAsync(id);
+            if (student is null) return false;
+
+            var result = hasher.VerifyHashedPassword(student, student.PasswordHash, password);
+            if (result == PasswordVerificationResult.Failed) return false;
+            return true;
+        }
+
+        public async Task UpdatePasswordAsync(int id, string password)
+        {
+            var student = await students.GetByIdAsync(id) ?? throw new KeyNotFoundException($"Студент с ID {id} не найден.");
+
+            var passwordHash = hasher.HashPassword(student, password);
+            await students.UpdatePasswordHashAsync(id, passwordHash);
+        }
+    }
+}
