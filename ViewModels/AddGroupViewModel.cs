aaf3e9b3c6bdb01b7dc33aa7e5aeb85e51c83181
Владислав <vlad2007gvv@gmail.com>
2026-06-21 20:42:09 +0300

    Добавлена модель для создания новой группы

---
+++ ViewModels/AddGroupViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalAccount.ViewModels;
+
+public class AddGroupViewModel
+{
+    [Required(ErrorMessage = "Введите название группы")]
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; } = string.Empty;
+
+}
