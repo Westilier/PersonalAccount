4bf108568d401cf554544647c95cfde1e325cf89
Владислав <vlad2007gvv@gmail.com>
2026-06-21 21:30:31 +0300

    Добавлена ViewModel для добавления дисциплины

---
+++ ViewModels/AddSubjectViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalAccount.ViewModels;
+
+public class AddSubjectViewModel
+{
+    [Required(ErrorMessage = "Введите название дисциплины")]
+    public string Name { get; set; } = string.Empty;
+}
